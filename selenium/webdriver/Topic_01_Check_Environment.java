9000d2637801a6756fc0a4f71ba600434d4664c9
Hieu Le <lengoduchieu@gmail.com>
2024-12-02 23:14:22 +0700

    Edit TC 02

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -18,6 +18,8 @@ public class Topic_01_Check_Environment {
     @Test
     public void TC_02_Run_On_Chrome() {
         driver = new ChromeDriver();
+
+
         driver.get("https://www.facebook.com/");
         driver.quit();
     }
