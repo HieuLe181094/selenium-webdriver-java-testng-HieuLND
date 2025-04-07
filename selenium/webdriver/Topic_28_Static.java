da0b8fbc4cd28f7f76a3fc1429e62d6355a41065
Hieu Le <lengoduchieu@gmail.com>
2025-04-07 16:19:39 +0700

    Update Static(Wait)

---
+++ selenium/webdriver/Topic_28_Static.java
@@ -0,0 +1,58 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_28_Static {
+
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void initialBrowser() {
+        driver = new FirefoxDriver();
+
+    }
+
+    @Test
+    public void TC_01_Less_Than() throws InterruptedException {
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+
+        driver.findElement(By.cssSelector("div#start>button")).click();
+        Thread.sleep(3000);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(),"Hello World!");
+    }
+
+    @Test
+    public void TC_02_Equal() throws InterruptedException {
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+
+        driver.findElement(By.cssSelector("div#start>button")).click();
+        Thread.sleep(5000);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(),"Hello World!");
+    }
+
+    @Test
+    public void TC_03_More_Than() throws InterruptedException {
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+
+        driver.findElement(By.cssSelector("div#start>button")).click();
+        Thread.sleep(10000);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(),"Hello World!");
+    }
+
+
+    @AfterClass
+    public void TC_Clean(){
+        driver.quit();}
+    }
